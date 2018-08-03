76121920a276bcb3cdfd4b520853f517b56a86ed
s-afanasiev <s-afanasiev@mail.ru>
2018-08-03 15:08:55 +0300

    ok

--- app/src/main/java/com/example/safanasyev/myapplication/data/DataMaker.java
+++ app/src/main/java/com/example/safanasyev/myapplication/data/DataMaker.java
@@ -3,7 +3,7 @@ package com.example.safanasyev.myapplication.data;
 public class DataMaker
 {
     private static final int DATASET_COUNT = 50;
-    
+
     public static String[] makeEmptyStringArray() {
         String[] mDataset = new String[DATASET_COUNT];
         for (int i = 0; i < DATASET_COUNT; i++) {
